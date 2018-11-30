6e481c075fc26ba7e6488ce51ae5bdb34e481782
SiqiCU <44420637+SiqiCU@users.noreply.github.com>
2018-11-30 01:24:38 -0500

    Update tk2_label_button.py

--- tkinterTUT/tk2_label_button.py
+++ tkinterTUT/tk2_label_button.py
@@ -6,12 +6,12 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title('my window')
-window.geometry('200x100')
+window.title('my window')##界面的名字
+window.geometry('200x100')##界面的大小
 
 var = tk.StringVar()
 l = tk.Label(window, textvariable=var, bg='green', font=('Arial', 12), width=15,
-             height=2)
+             height=2)##label 也是object, window 上面的一个label，bg=background, font=（字体，大小）
 #l = tk.Label(window, text='OMG! this is TK!', bg='green', font=('Arial', 12), width=15, height=2)
 l.pack()
 
@@ -28,6 +28,7 @@ def hit_me():
 b = tk.Button(window, text='hit me', width=15,
               height=2, command=hit_me)
 b.pack()
+##botton 是包里面一个object,做一个按钮，hit_me is a function , when you click hit_me, it will run hit_me function
 
-
-window.mainloop()
+window.mainloop()##mainloop 是while 循环， 不停的跟新，
+##l.pack()##安置
